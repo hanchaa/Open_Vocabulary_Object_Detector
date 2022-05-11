3045ae4fda0204d404bfbb0913e2a6936aabcfb9
JuHan <hanchaa@naver.com>
2022-05-11 16:54:10 +0900

    feat(ov-od): implement multi scale roi pooler

--- configs/zsod.py
+++ configs/zsod.py
@@ -1,7 +1,8 @@
 from detectron2.model_zoo import get_config
 from detectron2.config import LazyCall
 
-from model.Backbone.CLIPBackbone import CLIPBackbone
+from model.backbone.clip_backbone import CLIPBackbone
+from model.box_pooler.multi_scale_pooler import MultiScaleROIPooler
 
 default_configs = get_config("new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py")
 
@@ -14,3 +15,15 @@ model.backbone.update(
     in_features=["c2", "c3", "c4", "c5"]
 )
 
+[model.roi_heads.pop(k) for k in ["mask_in_features", "mask_pooler", "mask_head"]]
+
+model.roi_heads.update(
+    box_pooler=LazyCall(MultiScaleROIPooler)(
+        output_shape=7,
+        scales=(1.0 / 4, 1.0 / 8, 1.0 / 16, 1.0 / 32),
+        sampling_ratio=0,
+        pooler_type="ROIAlignV2"
+    )
+)
+
+print(model)
