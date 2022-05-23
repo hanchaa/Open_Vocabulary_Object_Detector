0d9609d16a6a1f844c661989aa1c7d1c4c4ae1b4
Juhan <hanchaa@gmail.com>
2022-05-23 22:54:08 +0900

    fix(ov-od): change tensor device to cuda

--- model/box_predictor/ov_classifier.py
+++ model/box_predictor/ov_classifier.py
@@ -17,12 +17,12 @@ class OpenVocabularyClassifier(nn.Module):
         input_dim = input_shape.channels * (input_shape.width or 1) * (input_shape.height or 1)
         self.projection = nn.Linear(input_dim, prompt_dim)
 
-        prompt_embedding = torch.tensor(np.load(prompt_path), dtype=torch.float32)
+        prompt_embedding = torch.tensor(np.load(prompt_path), dtype=torch.float32, device="cuda")
         self.prompt_embedding = normalize(prompt_embedding, dim=0)
         assert self.prompt_embedding.shape[0] == prompt_dim
         assert self.prompt_embedding.shape[1] == num_classes
 
-        self.background_embedding = torch.randn((prompt_dim, 1), requires_grad=True)
+        self.background_embedding = torch.randn((prompt_dim, 1), requires_grad=True, device="cuda")
 
         self.temperature = temperature
 
