e90ec61127c848dd00453da9d619cc350df07fef
Juhan <hanchaa@gmail.com>
2022-05-26 01:17:39 +0900

    fix(ov-od): predict probability using sigmoid if using bce loss

--- model/box_predictor/ov_fast_rcnn.py
+++ model/box_predictor/ov_fast_rcnn.py
@@ -129,3 +129,23 @@ class OVFastRCNNOutputLayers(FastRCNNOutputLayers):
             self.test_nms_thresh,
             self.test_topk_per_image,
         )
+
+    def predict_probs(self, predictions: Tuple[torch.Tensor, torch.Tensor], proposals: List[Instances]):
+        """
+        Args:
+            predictions: return values of :meth:`forward()`.
+            proposals (list[Instances]): proposals that match the features that were
+                used to compute predictions.
+        Returns:
+            list[Tensor]:
+                A list of Tensors of predicted class probabilities for each image.
+                Element i has shape (Ri, K + 1), where Ri is the number of proposals for image i.
+        """
+        scores, _ = predictions
+        num_inst_per_image = [len(p) for p in proposals]
+        if self.use_binary_ce:
+            probs = scores.sigmoid()
+        else:
+            probs = F.softmax(scores, dim=-1)
+
+        return probs.split(num_inst_per_image, dim=0)
