eab564297f5ab722c5dd709a6971c980cd35d531
Juhan <hanchaa@gmail.com>
2022-05-26 23:44:30 +0900

    chore(ov-od): transpose class embedding

--- model/box_predictor/ov_classifier.py
+++ model/box_predictor/ov_classifier.py
@@ -19,11 +19,11 @@ class OpenVocabularyClassifier(nn.Module):
         self.projection = nn.Linear(input_dim, prompt_dim)
 
         prompt_embedding = torch.tensor(np.load(prompt_path), dtype=torch.float, device="cuda")
-        self.prompt_embedding = normalize(prompt_embedding, dim=0)
-        assert self.prompt_embedding.shape[0] == prompt_dim
-        assert self.prompt_embedding.shape[1] == num_classes
+        self.prompt_embedding = normalize(prompt_embedding, dim=-1)
+        assert self.prompt_embedding.shape[0] == num_classes
+        assert self.prompt_embedding.shape[1] == prompt_dim
 
-        self.background_embedding = nn.Parameter(torch.randn((prompt_dim, 1))) if not use_binary_ce else None
+        self.background_embedding = nn.Parameter(torch.randn((1, prompt_dim))) if not use_binary_ce else None
 
         self.temperature = temperature
 
@@ -32,9 +32,9 @@ class OpenVocabularyClassifier(nn.Module):
         x = normalize(x, dim=-1)
 
         if self.background_embedding is not None:
-            classifier = torch.cat([self.prompt_embedding, normalize(self.background_embedding, dim=0)], dim=1)
+            classifier = torch.cat([self.prompt_embedding, normalize(self.background_embedding, dim=-1)], dim=0)
 
         else:
             classifier = self.prompt_embedding
 
-        return (x @ classifier) / self.temperature
+        return (x @ classifier.T) / self.temperature
